e8b9412d55c8850de26885d2786c8de8d0957461
Aniket Datta <2004aniketdatta@gmail.com>
2024-04-24 01:53:01 +0530

    Create dfs.py

---
+++ Python Algorithms/dfs.py
@@ -0,0 +1,20 @@
+graph={}
+
+def user_input():
+    n = int(input("Enter the number of nodes:"))
+    for _ in range(n):
+        key = input("Enter the node").upper()
+        elem = input("Enter the nodes connected to the node").split(",")
+        graph[key] = elem
+    return graph
+root = input("Enter the root node").upper()
+visited = set()
+def dfs(graph):
+    if root not in visited:
+        print(root)
+        visited.add(root)
+        for elem in graph[root]:
+            dfs(graph, elem)
+dfs_elements = dfs(graph)
+print(dfs_elements)
+            
