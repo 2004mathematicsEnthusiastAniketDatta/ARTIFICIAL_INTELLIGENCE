70a23d0e27c4f3b7d16014e503b3621844be4918
Aniket Datta <2004aniketdatta@gmail.com>
2024-05-27 22:25:04 +0530

    dfs

--- Python Algorithms/dfs.py
+++ Python Algorithms/dfs.py
@@ -1,20 +1,33 @@
-graph={}
+def dfs(graph, start):
+    visited = set()  # Set to keep track of visited nodes
+    stack = [start]  # Initialize the stack with the start node
 
-def user_input():
-    n = int(input("Enter the number of internal parent nodes with root node:"))
-    for _ in range(n):
-        key = input("Enter the node").upper()
-        elem = input("Enter the nodes connected to the node").split(",")
-        graph[key] = elem
-    return graph
-root = input("Enter the root node").upper()
-visited = set()
-def dfs(graph):
-    if root not in visited:
-        print(root)
-        visited.add(root)
-        for elem in graph[root]:
-            dfs(graph, elem)
-dfs_elements = dfs(graph)
-print(dfs_elements)
-            
+    while stack:
+        node = stack.pop()  # Pop a node from the stack
+        if node not in visited:
+            print(node)  # Process the node (in this case, print it)
+            visited.add(node)  # Mark the node as visited
+            # Add all unvisited neighbors to the stack
+            stack.extend(neighbor for neighbor in graph[node] if neighbor not in visited)
+
+# Input graph and start node
+def main():
+    graph = {}
+    print("Enter the graph as adjacency list (press Enter after each node):")
+    while True:
+        try:
+            line = input().strip()
+            if line == "":
+                break
+            node, *neighbors = line.split()
+            graph[node] = neighbors
+        except EOFError:
+            break
+
+    start_node = input("Enter the start node: ").strip()
+    
+    print("DFS Traversal:")
+    dfs(graph, start_node)
+
+if __name__ == "__main__":
+    main()
